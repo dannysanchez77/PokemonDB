bfe67eae9254ea2e82121de2e5de52ed5b732742
Danny <dannysanchez77@hotmail.es>
2022-02-09 10:58:25 +0100

    todo

--- demo/src/main/kotlin/com/example/demo/Database.kt
+++ demo/src/main/kotlin/com/example/demo/Database.kt
@@ -1,9 +1,45 @@
 package com.example.demo
 
+import org.springframework.boot.CommandLineRunner
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 
 @Configuration
 class Database {
+    @Bean
+    fun initDatabase(pokemonRepository: PokemonRepository):CommandLineRunner{
+        return CommandLineRunner {
+            println("Base de datos creandose")
+            val listaPokemon = listOf(
+                Pokemon("Charmander",1),
+                Pokemon("Bulbasaur",1),
+                Pokemon("Squirtel",1)
+            )
+            listaPokemon.forEach{
+                println("El Pokemon ${it.nombre} tiene un id de ${it.id} ")
+            }
+            listaPokemon.forEach {
+                pokemonRepository.save(it)
+            }
+            val p = Pokemon("Ratata",1)
+            pokemonRepository.save(p)
 
+            p.nivel = 2
+            pokemonRepository.save(p)
+
+            p.id = 10
+            pokemonRepository.save(p)
+
+
+
+
+
+
+            pokemonRepository.findAll().forEach{
+                println(it)
+            }
+            println("La base de datos esta creada y llena de datos")
+        }
+    }
 }
--- demo/src/main/kotlin/com/example/demo/PokemonController.kt
+++ demo/src/main/kotlin/com/example/demo/PokemonController.kt
@@ -1,35 +1,64 @@
 package com.example.demo
 
+import com.google.gson.Gson
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import kotlin.random.Random
 
 @RestController
-class PokemonController (private val pokemonRepository: PokemonRepository){
+class PokemonController (private val pokemonRepository: PokemonRepository) {
 
     @GetMapping("prueba1")
     fun prueba1() {
         println("En la base de datos hay ${pokemonRepository.count()}")
-        val pokemon = Pokemon("P1",1)
+        val pokemon = Pokemon("P1", 1)
         pokemonRepository.save(pokemon)
         println("En la base de datos ahora hay ${pokemonRepository.count()}")
     }
+
     @GetMapping("mostrarPokemon")
-    fun mostrarPokemon() : MutableList<Pokemon> {
+    fun mostrarPokemon(): MutableList<Pokemon> {
         return pokemonRepository.findAll()
     }
 
-    @GetMapping("actualizarPokemon")
-    fun actualizarPokemon() {
-        var p =Pokemon("Pokemon Reemplazado",99)
-        p.id =1
-        pokemonRepository.save(p)
+    @GetMapping("actualizarPokemon/{id}/{nuevoNivel}")
+    fun actualizarPokemon(@PathVariable id: Int, @PathVariable nuevoNivel: Int) {
+        val posiblePokemon = pokemonRepository.findById(id)
+        if (posiblePokemon.isPresent) {
+            val pokemon = posiblePokemon.get()
+            pokemon.nivel = nuevoNivel
+            pokemonRepository.save(pokemon)
+        }
+        pokemonRepository.findAll().forEach { println(it) }
     }
+
     @GetMapping("insertarPokemon/{nombre}/{nivel}")
-    fun insertarPokemon(@PathVariable nombre:String, @PathVariable nivel:Int): Pokemon {
-        val pokemon =Pokemon(nombre, nivel)
+    fun insertarPokemon(@PathVariable nombre: String, @PathVariable nivel: Int): Pokemon {
+        val pokemon = Pokemon(nombre, nivel)
         pokemonRepository.save(pokemon)
         return pokemon
+    }
+
+    @GetMapping("getPokemonById/{pokemonId}")
+    fun getPokemonById(@PathVariable pokemonId: Int): Pokemon {
+        val pokemon = pokemonRepository.getById(pokemonId)
+        return pokemon
+    }
+
+    @GetMapping("insertPokemonBody")
+    fun insertPokemonBody(@RequestBody texto: String) {
+        val gson = Gson()
+        val pokemon = gson.fromJson(texto, Pokemon::class.java)
+        pokemonRepository.save(pokemon)
+        pokemonRepository.findAll().forEach { println(it) }
+    }
+    @GetMapping("insertPokemonJson")
+    fun insertPokemonJson(@RequestBody texto: String) {
+        val gson = Gson()
+        val pokemon = gson.fromJson(texto, Pokemon::class.java)
+        pokemonRepository.save(pokemon)
+        pokemonRepository.findAll().forEach { println(it) }
     }
 }
